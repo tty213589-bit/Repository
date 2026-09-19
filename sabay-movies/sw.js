bdf4282d2423ab01f088decd38b04334d7d0cd2d
tty213589-bit <tty213589@gmail.com>
2026-09-19 23:56:27 +0700

    Add Sabay Movies service worker

---
+++ sabay-movies/sw.js
@@ -0,0 +1,1 @@
+const CACHE="sabay-movies-v1";const ASSETS=["./","./index.html","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
